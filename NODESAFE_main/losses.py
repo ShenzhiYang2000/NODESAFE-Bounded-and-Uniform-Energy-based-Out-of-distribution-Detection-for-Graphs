bb79cda00b29c76d20f74832eb64822524133b2d
Shenzhi Yang <73417792+ShenzhiYang2000@users.noreply.github.com>
2024-10-10 19:06:22 +0800

    Update losses.py

--- NODESAFE_main/losses.py
+++ NODESAFE_main/losses.py
@@ -7,9 +7,9 @@ import torch.nn.functional as F
 
 
 class UB_loss(nn.Module):
-    def __init__(self, lamda1 = 0.999):
+    def __init__(self, margin=0.01):
         super(UB_loss, self).__init__()
-        self.lamda1 = lamda1
+        self.margin = margin
 
     def forward(self, _features, labels=None, _features_out=None, epoch=None):
 
@@ -21,30 +21,34 @@ class UB_loss(nn.Module):
         modulus_mean = torch.abs(torch.mean(modulus)).detach_()
         modulus_var = torch.var(modulus)
 
+
         Sum = _features.sum(dim=1)
         Sum_mean = torch.mean(Sum).detach_()
         Sum_var = torch.var(Sum)
+
 
         if _features_out is not None:
             Sum_out = _features_out.sum(dim=1)
             Sum_mean_out = torch.abs(torch.mean(Sum_out)).detach_()
             Sum_var_out = torch.var(Sum_out)
 
+        alpha = 0.999
 
         if _features_out is not None:
             if epoch > 120:
-                modulus_loss = self.lamda1 * modulus_var / modulus_mean  + (1-self.lamda1) * Sum_var / Sum_mean + (1-self.lamda1) * Sum_var_out / Sum_mean_out
+                modulus_loss = alpha * modulus_var / modulus_mean  + (1-alpha) * Sum_var / Sum_mean + (1-alpha) * Sum_var_out / Sum_mean_out
             else:
-                modulus_loss = self.lamda1 * modulus_var / modulus_mean  
+                modulus_loss = alpha * modulus_var / modulus_mean  
 
         else:
             if epoch > 50:
-                modulus_loss = self.lamda1 * modulus_var / modulus_mean + (1-self.lamda1) * Sum_var / Sum_mean 
+                modulus_loss = alpha * modulus_var / modulus_mean + (1-alpha) * Sum_var / Sum_mean 
+
             else:
                 modulus_loss = 0
 
-        return modulus_loss, modulus_mean 
 
+        return modulus_loss, modulus_mean 
 
 
 
